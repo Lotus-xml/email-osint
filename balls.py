863b27f9584e792272f8b7c1d8050f8e9616f79d
Lotus <92619986+Lotus-xml@users.noreply.github.com>
2023-03-06 13:40:59 -0500

    added the file idiot

---
+++ balls.py
@@ -0,0 +1,31 @@
+import os
+import sys
+try:
+    import holehe
+except ImportError:
+    print("Holehe not found. Exiting! (this means install it retard)")
+    exit()
+
+def giantcock():
+   if sys.platform == "linux":
+    os.system("clear")
+   elif sys.platform == "win32":
+    os.system("cls")
+giantcock()
+
+femboy = "Email Osint | Made by Lotus"
+print(f'\33]0;{femboy}\a', end='', flush=True)
+
+#too lazy to add google people api into this so do it yourself lmao
+
+sus = input("Enter the target email: ")
+giantcock()
+print("Holehe:")
+os.system(f'holehe --only-used {sus}')
+if "@gmail.com" in sus:
+  #print(f"Google ID: {id}")
+  #gayglemaps = f"https://www.google.com/maps/contrib/{id}"
+  cock = f"https://calendar.google.com/calendar/u/7/embed?src={sus}"
+  print(f"\nGoogle Calendar: {cock}")
+else:
+  pass
